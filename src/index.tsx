b59dac4a62addff791819312a48bd912a0458987
Chclt <realchclt@gmail.com>
2025-01-02 13:16:24 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { AppProvider } from "@/lib/hooks/appProvider.tsx";
 import { DatabaseProvider } from "@/lib/hooks/databaseProvider";
 import { WorkerProvider } from "@/lib/hooks/workerProvider.tsx";
+import { ErrorBoundary } from "react-error-boundary";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
@@ -12,11 +13,19 @@ if (rootEl) {
   root.render(
     <React.StrictMode>
       <AppProvider>
-        <WorkerProvider>
-          <DatabaseProvider>
-            <App />
-          </DatabaseProvider>
-        </WorkerProvider>
+        <ErrorBoundary
+          fallback={
+            typeof Worker === "undefined" && (
+              <p>你的浏览器不支持 Web Worker，请检查浏览器版本或浏览器设置</p>
+            )
+          }
+        >
+          <WorkerProvider>
+            <DatabaseProvider>
+              <App />
+            </DatabaseProvider>
+          </WorkerProvider>
+        </ErrorBoundary>
       </AppProvider>
     </React.StrictMode>,
   );
